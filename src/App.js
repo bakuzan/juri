d577fcd2abbd2b6435ddfcfb95fbc57cb547341c
bakuzan <imannoyed4days@hotmail.co.uk>
2016-12-01 21:20:42 +0000

    search box component in progress, added .env

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <h2>JURI</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, search for something.
         </p>
         <SearchBox />
       </div>
--- src/components/search-box/search-box.js
+++ src/components/search-box/search-box.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 
-class SearchBox extends React.Component {
-
+class SearchBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchString: ''
+    };
+  }
+  handleChange(event) {
+    this.setState({
+      searchString: event.target.value
+    });
+  }
+  render() {
+    return (
+      <div className="search-box">
+        <input type="text"
+               value={this.state.searchString}
+               onChange={(e) => this.handleChange(e)} />
+      </div>
+    );
+  }
 }
 
 export default SearchBox;
