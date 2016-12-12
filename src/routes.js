b85526fe97fdfc94fbe3d36fb23a7f00b038aff6
bakuzan <imannoyed4days@hotmail.co.uk>
2016-12-12 15:26:11 +0000

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import App from './views/app/app';
 import About from './views/about/about';
+import FilteredSearchResult from './components/filtered-search-result/filtered-search-result.js';
 import {Router, Route, browserHistory} from 'react-router';
 
 class Routes extends Component {
   render() {
     return (
       <Router history={browserHistory}>
-        <Route path="/" component={App}/>
-        <Route path="/about" component={About}/>
+        <Route path="/" component={App}>
+          <IndexRoute component={FilteredSearchResult} />
+          <Route path="about" component={About} />
+        </Route>
       </Router>
     );
   }
