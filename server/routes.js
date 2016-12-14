0b471a9833283cfa9dfbc92bddf0c8dd32dd33e1
bakuzan <imannoyed4days@hotmail.co.uk>
2016-12-14 09:17:18 +0000

    Update routes.js
    route declared by constant

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,7 @@
 import mal from './mal-search';
+import paths from '../src/constants/paths';
 
 export const routes = (app) => {
-
-	// MAL Routes
-	app.route('/mal-search/:type').get(mal.search);
+  // MAL Routes
+  app.route(paths.query.malSearch).get(mal.search);
 }
