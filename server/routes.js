2938c40adfa01bb43c773992eaca584588c6fb88
bakuzan <imannoyed4days@hotmail.co.uk>
2016-12-15 10:53:35 +0000

    Update routes.js
    separated routes from app.js

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,23 @@
-const app = require('./app');
-const malSearch = require('./mal-search');
+const express = require('express');
+const mal = require('./mal-search');
 const paths = require('../src/constants/paths');
 
-export const routes = () => {
-  // MAL Route
-  app.route(paths.query.malSearch).get(mal.search);
+const router = express.Router(); 
 
-  // Content Site Routes
-}
+// middleware to use for all requests
+router.use((req, res, next) => {
+    console.log('Query fired! : ', req.url);
+    next(); // next route
+});
+
+//MAL route
+router.get(paths.query.malSearch, mal.search);
+
+// Content Site Routes
+
+// Always return the main index.html, so react-router render the route in the client ## /^\/(?!api)/g
+router.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
+module.exports = router;
