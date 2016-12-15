dc977116ed3c7cfb031c13778320db66c0888353
bakuzan <imannoyed4days@hotmail.co.uk>
2016-12-15 10:06:06 +0000

    Update paths.js
    removed /api from routes, will be added by server ( app.use('/api', router) )

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -10,7 +10,7 @@ export const paths = {
   base: '/juri/',
   about: 'about',
   query: {
-    malSearch: '/api/mal-search/:type',
-    siteSearch: '/api/site-search/:type/:age'
+    malSearch: '/mal-search/:type',
+    siteSearch: '/site-search/:type/:age'
   }
 };
