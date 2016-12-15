5c76c226a6367b8a153f3b40db8d9b2c7a8d34ec
bakuzan <imannoyed4days@hotmail.co.uk>
2016-12-15 10:53:50 +0000

    Update app.js
    moved all routes to routes.js

--- server/app.js
+++ server/app.js
@@ -7,14 +7,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const router = express.Router(); 
-
-// middleware to use for all requests
-router.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
-});
 
 // Setup logger
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
@@ -22,12 +14,7 @@ app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:htt
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
-// Always return the main index.html, so react-router render the route in the client ## /^\/(?!api)/g
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
-});
-
-// Register server queries prefixed with /api
-app.use('/api', router);
+// Routes
+app.use(require('./routes'));
 
 module.exports = app;
