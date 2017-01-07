76dfeabe81cea366514fea69ee8245c28a227466
bakuzan <imannoyed4days@hotmail.co.uk>
2017-01-07 22:03:13 +0000

    sprucing up about page.

--- src/views/about/about.js
+++ src/views/about/about.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { paths } from '../../constants/paths';
+import './about.css';
 
 class About extends Component {
   render() {
@@ -8,15 +9,21 @@ class About extends Component {
       <article className="about">
         <header>
           <h2>Project JURI</h2>
+          <p>Juri is a unified search application.</p>
         </header>
-        <div>
+        <div className="content">
           <p>
-            This page will be for explaining project JURI, displaying version numbers and such.
-            <br /><br />
-            <b>Status:</b> Work in progress.
-            <br /><br />
-            <Link to={paths.base}>back</Link>
+            Using her Feng Shui engine, Juri is able to retrieve the myanimelist,
+            and specified content site entires for the search term and categories entered.
+            Further sites can be queried using the site dropdown list, which will append additional results.
           </p>
+          <p>
+            Juri also provides a latest releases page, powered by
+             <a href="https://www.masterani.me/">masterani</a> and <a href="http://mangafox.me/releases">mangafox</a>
+            for anime and manga respectively.
+          </p>
+          <Link to={paths.base}>Home</Link>
+          <Link to={paths.latest}>Latest</Link>
         </div>
       </article>
     );
