b38a41a5ba6bd5ce956ec56a47cff83e0c4a0b8e
bakuzan <imannoyed4days@hotmail.co.uk>
2017-01-07 21:48:25 +0000

    adjust ports for quick switching

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.SERVER_PORT || 9000;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
