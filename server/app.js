ed30c5e4407e0d3d501043c523161a3856032256
bakuzan <imannoyed4days@hotmail.co.uk>
2017-04-12 22:00:53 +0100

    added express route for a run in "production" mode

--- server/app.js
+++ server/app.js
@@ -12,9 +12,16 @@ const app = express();
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
 // Serve static assets
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use('/juri/static', express.static(path.resolve(__dirname, '..', 'build/static')));
 
 // Routes
 app.use(require('./routes'));
+
+// Always return the main index.html, so react-router render the route in the client
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+  });
+}
 
 module.exports = app;
