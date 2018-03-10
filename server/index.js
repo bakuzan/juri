b751cd7c5cced32a529087c67c5ff86fdee19b4e
bakuzan <imannoyed4days@hotmail.co.uk>
2018-03-10 12:22:21 +0000

    changes to allow iisnode hosting.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 'use strict';
 
+const Constants = require('./constants');
 const app = require('./app');
 
-const PORT = process.env.SERVER_PORT || 9000;
+const PORT =
+  process.env.NODE_ENV === Constants.environment.production
+    ? process.env.PORT
+    : 9000;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
