ed31c432d47e389229dac5135d1d959e0eb93b85
bakuzan <imannoyed4days@hotmail.co.uk>
2022-01-29 07:25:44 +0000

    Dependencies, node-fetch hack

--- server/utils/juriFetch.js
+++ server/utils/juriFetch.js
@@ -1,5 +1,5 @@
 // const got = require('got');
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 // const { catchCloudflare } = require('@ctrl/cloudflare');
 // const { CookieJar } = require('tough-cookie');
 
@@ -24,6 +24,8 @@ const fetch = require('node-fetch');
 // }
 
 module.exports = async function juriFetch(opts, reqOpts) {
+  const fetch = (...args) =>
+    import('node-fetch').then(({ default: nodeFetch }) => nodeFetch(...args));
   return await fetch(opts.url, { method: 'GET', ...reqOpts });
   // const bypassCloudflare = opts.cloudflare || false;
 
